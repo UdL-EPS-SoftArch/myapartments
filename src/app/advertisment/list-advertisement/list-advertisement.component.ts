69d8440694d799243b22776769e69bc79b1056c2
Josep Prades Vidal <jus25gamer@gmail.com>
2024-11-26 16:30:06 +0100

    Initial commit list-adverisment.component.css

---
+++ src/app/advertisment/list-advertisement/list-advertisement.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-list-advertisment',
+    templateUrl: './list-advertisment.component.html',
+    styleUrls: ['./list-advertisment.component.css']
+  })
+
+export class AboutComponent {
+  constructor() { }
+
+}
