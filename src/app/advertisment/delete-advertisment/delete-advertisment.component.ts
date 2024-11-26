4380212033bba0ee4d46b2c6201aff4ebe2d02e2
Adrià Creus Nuñez <adriacreus03@gmail.com>
2024-11-26 16:08:51 +0100

    Ts

---
+++ src/app/advertisment/delete-advertisment/delete-advertisment.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-delete-advertisment',
+  templateUrl: './delete-advertisment.component.html',
+  styleUrls: ['./delete-advertisment.component.css']
+})
+export class DeleteAdvertismentComponent {
+
+  constructor() { }
+
+}
