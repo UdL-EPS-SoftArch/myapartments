36c6a2c16272e6e9eeceeac1a689f65a5ba5478e
Josep Prades Vidal <jus25gamer@gmail.com>
2024-12-04 17:28:02 +0100

    Create advertisment-list.component.ts

---
+++ src/app/advertisment/advertisment-list/advertisment-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advertisment-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './advertisment-list.component.html',
+  styleUrl: './advertisment-list.component.css'
+})
+export class AdvertismentListComponent {
+
+}
