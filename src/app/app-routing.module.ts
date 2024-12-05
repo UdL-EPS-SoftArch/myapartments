3b334ad5e93979e2ef055bd209ea35a4d2b1b18e
Sergi Vila Monguia <47522460+Troter2@users.noreply.github.com>
2024-12-05 16:24:25 +0100

    added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
 import {ApartmentListComponent} from './apartment/apartment-list/apartment-list.component';
 import {ApartmentCreateComponent} from './apartment/apartment-create/apartment-create.component';
+import { CreateAdvertisementComponent } from './advertisement/create-advertisement/create-advertisement.component';
 
 const routes: Routes = [
   { path: 'users/create', component: UserRegisterComponent},
@@ -20,6 +21,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent},
   {path: 'apartments', component: ApartmentListComponent},
   {path: 'apartment/create', component: ApartmentCreateComponent},
+  {path: 'advertisement/create', component: CreateAdvertisementComponent},
   { path: '404', component: NotFoundComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full'},
 ];
