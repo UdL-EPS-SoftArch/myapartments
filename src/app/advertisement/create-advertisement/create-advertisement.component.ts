8a4106c1cac0c952e21479ff778c80fd5670c7fb
Sergi Vila Monguia <47522460+Troter2@users.noreply.github.com>
2024-12-05 16:11:05 +0100

    Create Create-apartmen componente

---
+++ src/app/advertisement/create-advertisement/create-advertisement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-advertisement',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-advertisement.component.html',
+  styleUrl: './create-advertisement.component.css'
+})
+export class CreateAdvertisementComponent {
+
+}
