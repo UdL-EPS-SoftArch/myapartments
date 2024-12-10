00e7ac9e7eccb53428a91613378f6a10f451e043
Sergi Vila Monguia <47522460+Troter2@users.noreply.github.com>
2024-12-10 15:34:25 +0100

    create Advertisement class

---
+++ src/app/advertisement/advertisement.ts
@@ -0,0 +1,24 @@
+import { HateoasResource, Resource } from "@lagoshny/ngx-hateoas-client";
+import { Apartment } from "../apartment/apartment";
+import { AdvertisementStatus } from "../advertisement-status/advertisementStatus";
+
+@HateoasResource('advertisement')
+export class Advertisement extends Resource {
+    id: number = 0; 
+    title: string = ''; 
+    description: string = ''; 
+    price: number = 0.01; 
+    zipCode: string = ''; 
+    country: string = ''; 
+    address: string = ''; 
+    creationDate: Date = new Date(); 
+    expirationDate?: Date; 
+  
+    adStatus: AdvertisementStatus | number = 0; 
+    apartment: Apartment | number = 0; 
+  
+    constructor(values: object = {}) {
+        super();
+        Object.assign(this as any, values);
+    }
+}
