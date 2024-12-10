f1bf02c2320f7bb62f0c043ede914ef765397edf
Josep Prades Vidal <jus25gamer@gmail.com>
2024-12-10 15:49:16 +0100

    add a mock for apartment

--- src/app/advertisment/advertisment-list/advertisment-list.component.ts
+++ src/app/advertisment/advertisment-list/advertisment-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from '../../login-basic/user';
+import { Apartment } from '../../apartment/apartment';
+import { ApartmentService } from '../../apartment/apartment.service';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-advertisment-list',
@@ -7,6 +11,42 @@ import { Component } from '@angular/core';
   templateUrl: './advertisment-list.component.html',
   styleUrl: './advertisment-list.component.css'
 })
-export class AdvertismentListComponent {
 
+export interface Advertisement {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  zipCode: string;
+  country: string;
+  address: string;
+  creationDate: string;
+  expirationDate?: string;
+  adStatus: string;
+  apartment: number;
 }
+
+export class AdvertismentListComponent {
+  apartments: Apartment[] = [];
+  user: User = new User();
+
+  constructor(
+    private apartmentService: ApartmentService,
+    private authenticationService: AuthenticationBasicService
+  ) {}
+
+  ngOnInit() {
+    this.user = this.authenticationService.getCurrentUser();
+    this.apartmentService.findByOwner(this.user).subscribe({
+      next: (response) => {
+        this.apartments = response.resources;
+      },
+      error: (err) => {
+        console.error('Error al cargar apartamentos:', err);
+      }
+    });
+    // This is a mock
+    this.apartments[0] = new Apartment();
+  }
+}
+
