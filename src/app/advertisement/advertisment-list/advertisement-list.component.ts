a13616ceb189701bafb297926ced4904ddf8379a
Josep Prades Vidal <jus25gamer@gmail.com>
2024-12-11 17:26:28 +0100

    Added the advertisement list

---
+++ src/app/advertisement/advertisment-list/advertisement-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Advertisement } from '../advertisement';
+import { AdvertisementService } from '../advertisement.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-advertisement-list',
+  standalone: true,
+  imports:[CommonModule],
+  templateUrl: './advertisement-list.component.html',
+  styleUrls: ['./advertisement-list.component.css']
+})
+export class AdvertisementListComponent implements OnInit {
+  advertisements: Advertisement[] = [];
+
+  constructor(private advertisementService: AdvertisementService) {}
+
+  ngOnInit() {
+    this.loadAdvertisements();
+  }
+
+  loadAdvertisements() {
+    this.advertisementService.getAllAdvertisements().subscribe({
+      next: (response: any) => {
+        this.advertisements = response.resources;
+      },
+      error: (err: any) => {
+        console.error('Error al cargar los anuncios:', err);
+      }
+    });
+  }
+}
