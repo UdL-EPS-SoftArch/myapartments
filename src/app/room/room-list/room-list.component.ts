909419ddbcbf6038407a64c512f768577167bc2f
Aniol0012 <aniol0012@gmail.com>
2024-12-12 17:25:35 +0100

    feat: add room list component logic

--- src/app/room/room-list/room-list.component.ts
+++ src/app/room/room-list/room-list.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RoomService } from '../room.service';
+import { Room } from '../room';
+import { Apartment } from '../../apartment/apartment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-room-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './room-list.component.html',
-  styleUrl: './room-list.component.css'
+  styleUrls: ['./room-list.component.css']
 })
-export class RoomListComponent {
+export class RoomListComponent implements OnInit {
+  @Input() apartment!: Apartment;
+  // Max number of rooms to show, set to -1 to show all
+  @Input() maxRooms: number = 10;
+  rooms: Room[] = [];
 
+  constructor(private roomService: RoomService) {}
+
+  ngOnInit(): void {
+    if (this.apartment) {
+      this.roomService.findByApartment(this.apartment).subscribe({
+        next: (collection) => {
+          if (this.maxRooms === -1) {
+            this.rooms = collection.resources;
+          } else {
+            this.rooms = collection.resources.slice(0, this.maxRooms);
+          }
+        },
+        error: (err) => console.error('Error fetching rooms:', err),
+      });
+    }
+  }
 }
