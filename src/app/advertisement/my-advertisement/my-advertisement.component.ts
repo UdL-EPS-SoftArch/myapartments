d1dfb12dd177408d00efe7381d774a2261f63f8c
MoreTheo <theomorenolomero@gmail.com>
2024-12-14 21:54:52 +0100

    add new component

---
+++ src/app/advertisement/my-advertisement/my-advertisement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-advertisement',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-advertisement.component.html',
+  styleUrl: './my-advertisement.component.css'
+})
+export class MyAdvertisementComponent {
+
+}
