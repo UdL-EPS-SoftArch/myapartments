f96f8d833aa86ea02e4cd14e246fc487786be0eb
Aniol <aniol0012@gmail.com>
2024-12-19 16:47:49 +0100

    fix: removed unused input

--- src/app/room/room-list/room-list.component.ts
+++ src/app/room/room-list/room-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RoomService } from '../room.service';
 import { Room } from '../room';
