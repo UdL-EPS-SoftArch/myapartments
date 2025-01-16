a47e39751c16412fc1211bb3c615dd0cd33d4991
samplecode17 <ibrahim_mou17@hotmail.com>
2025-01-16 23:27:02 +0100

    feat: first create-room test implementation

--- e2e/features/Room/CreateRoom.feature
+++ e2e/features/Room/CreateRoom.feature
@@ -1,21 +1,17 @@
 Feature: Create a new Room
-    In order to use the app
-    As admin
-    I want to create a new room
+  In order to use the app
+  As admin
+  I want to create a new room
 
-    Scenario: Create a new room 
-        Given I'm in the homepage logged in as an owner with username "owner" and password "password"
-        When I go to the room list page
-        And I click on create room button
-        And I select "This apartment is static DO NOT TOUCH" for "Select apartment"
-        And I select "True" for "Is occupied?"
-        And I select "True" for "Has bed?"
-        And I select "True" for "Has window?"
-        And I select "True" for "Has desk?"
-        And I enter "45" into the "Surface" field
-        And I click the "Submit" button
-        Then I've created a new room with creation user "owner", apartment "This apartment is static DO NOT TOUCH", is occupied "True", has bed "True", has window "True", has desk "True" and Surface"45"
-    Scenario: Create a new room with logged in as user
-        Given I'm in the homepage logged in as user with username "demo" and password "password"
-        When I click on the "Room" menu option with class ".nav-link"
-        Then I try to click on "#createRoom" element
+  Scenario: Create a new room 
+    Given I'm in the homepage logged in as owner with username "owner" and password "password"
+    When  I go to the room list page
+    And I click on create room button
+    And I select "This apartment is static DO NOT TOUCH" for "Select apartment"
+    And I select "True" for "Is occupied?"
+    And I select "True" for "Has bed?"
+    And I select "True" for "Has window?"
+    And I select "True" for "Has desk?"
+    And I enter "45" into the "Surface" field
+    And I click the "Submit" button
+    Then I've created a new room with creation user "owner", apartment "This apartment is static DO NOT TOUCH", is occupied "True", has bed "True", has window "True", has desk "True" and Surface"45"
--- e2e/steps/Room/create-room.ts
+++ e2e/steps/Room/create-room.ts
@@ -1,52 +1,63 @@
 import {Given, When, Then, And} from 'cypress-cucumber-preprocessor/steps';
-import { error } from 'console';
+import { DataTable } from '@cucumber/cucumber';
 
-
-Given('I\'m in the homepage logged in as owner with username {string} and password {string}', (username,password) => {
+Given('I\'m in the homepage logged in as owner with username {string} and password {string}', (username, password) => {
     cy.visit('http://localhost:4200');
     cy.get('.nav-link').contains('Login').click();
-    cy.get('input[name=username]').type(username);
-    cy.get('input[name=password]').type(password);
-    cy.get('button[type=submit]').click()
-})
+    cy.get('#username').type(username).blur();
+    cy.get('#password').type(password).blur();
+    cy.get('button').contains('Submit').click();
+});
+
 
 When(/^I go to the room list page$/, function () {
     cy.get('.nav-link').contains('Rooms').click();
 });
 
-When(/^I click on create room button$/, function () {
+And(/^I click on create room button$/, function () {
     cy.get('button.btn.btn-primary.mt-3.mb-3')
     .contains('Create')
     .click(); 
 });
 
-When('I select {string} for {string}', (value: string, fieldName: 'Select apartment' | 'Is occupied?' | 'Has bed?' | 'Has window?' | 'Has desk?') => {
-    const dropdownSelectors: { [key: string]: string } = {
-      "Select apartment": '#apartmentSelect',
-      "Is occupied?": '#trueFalseSelectOcupied',
-      "Has bed?": '#trueFalseSelectHasBed',
-      "Has window?": '#trueFalseSelectHasWindow',
-      "Has desk?": '#trueFalseSelectHasDesk',
-    };
-  
-    // Usamos el índice con seguridad porque fieldName está tipado
-    cy.get(dropdownSelectors[fieldName], { timeout: 10000 })
-      .should('be.visible')
-      .select(value);
-  });
+And('I select "This apartment is static DO NOT TOUCH" for "Select apartment"', () => {
+    cy.get('#apartmentSelect').select('This apartment is static DO NOT TOUCH');
+});
 
-  When('I enter {string} into the {string} field', (value: string, fieldName: 'Surface') => {
-    const fieldSelectors = {
-      "Surface": '#surfaceInput',
-    };
+And('I select "True" for "Is occupied?"', () => {
+    cy.get('#trueFalseSelectOcupied').select('True');
+}); 
+
+And('I select "True" for "Has bed?"', () => {
+    cy.get('#trueFalseSelectHasBed').select('True');
+});
+
+And('I select "True" for "Has window?"', () => {
+    cy.get('#trueFalseSelectHasWindow').select('True');
+});
+
+And('I select "True" for "Has desk?"', () => {
+    cy.get('#trueFalseSelectHasDesk').select('True');
+});
+
+And('I enter "45" into the "Surface" field', () => {
+    cy.get('#surfaceInput').type('45');
+});
+
+And('I click the {string} button', (buttonName) => {
+cy.contains('button', buttonName)
+    .should('be.visible')
+    .click();
+});
+Then('I should see a new room with apartment "This apartment is static DO NOT TOUCH", is occupied "True", has bed "True", has window "True", has desk "True" and Surface "45"', () => {
+    cy.get('table').should('be.visible');
+    cy.get('table tbody tr').last().within(() => {
+      cy.get('td').eq(1).should('contain.text', 'This apartment is static DO NOT TOUCH');
+      cy.get('td').eq(2).should('contain.text', 'true');
+      cy.get('td').eq(3).should('contain.text', 'true');
+      cy.get('td').eq(4).should('contain.text', 'true');
+      cy.get('td').eq(5).should('contain.text', 'true');
+      cy.get('td').eq(6).should('contain.text', '45');
+    });
+  });
   
-    cy.get(fieldSelectors[fieldName])
-      .should('be.visible')
-      .clear()
-      .type(value);
-  });
-  When('I click the {string} button', (buttonName) => {
-    cy.contains('button', buttonName)
-      .should('be.visible')
-      .click();
-  });
